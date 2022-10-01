e993eb48a4255f7446a5250eed1c303a0230f40a
beucodi <odinbeuchat.ob@ikmail.com>
2022-10-01 19:24:56 +0200

    better styling

--- pages/index.vue
+++ pages/index.vue
@@ -4,15 +4,17 @@
 
 <template>
 	<div class="bg-slate-300 dark:bg-slate-800 dark:text-white h-screen">
-		<h1>Short</h1>
-		<p>Welcome! This website is used mostly by myself for shortening links I use daily!</p>
-		<p>You can have a look around, here are the availables codes !</p>
-		<nav>
-			<ul>
-				<li>
-					<NuxtLink to="/vsc">vsc - Visual Studio Code installer</NuxtLink>
-				</li>
-			</ul>
-		</nav>
+		<div class="pt-5 w-100 lg:w-4/5 m-auto flex flex-col items-center">
+			<h1>Short</h1>
+			<p class="mb-2">Welcome! This website is used mostly by myself for shortening links I use daily!</p>
+			<p class="mb-2">You can have a look around, here are the availables codes !</p>
+			<nav>
+				<ul>
+					<li>
+						<NuxtLink to="/vsc">vsc - Visual Studio Code installer</NuxtLink>
+					</li>
+				</ul>
+			</nav>
+		</div>
 	</div>
 </template>

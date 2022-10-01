6db9e935b006e4144436872c78411b5558fe8933
beucodi <odinbeuchat.ob@ikmail.com>
2022-10-01 18:57:39 +0200

    better tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 	modules: [
-		'@nuxtjs/tailwindcss'
-	]
+		'@nuxtjs/postcss8'
+	],
+	css: [
+		'@/assets/css/main.css',
+	],
+	build: {
+		postcss: {
+			postcssOptions: {
+				plugins: {
+					tailwindcss: {},
+					autoprefixer: {},
+				}
+			}
+		},
+	}
 });

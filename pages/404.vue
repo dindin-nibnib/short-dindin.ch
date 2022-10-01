d063a48bfe54b31ddf05951dccacb7563ee3498a
beucodi <odinbeuchat.ob@ikmail.com>
2022-10-01 19:19:52 +0200

    amend (fixed 404)

--- pages/404.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<div class="h-screen bg-slate-300 dark:bg-slate-800 dark:text-white flex items-center justify-center">
-		<div class="w-100 h-fit">
-			<h1 class="w-fit m-auto">404</h1>
-			<p class="w-fit m-auto">Page not found</p>
-			<NuxtLink to="/">Get me back home!</NuxtLink>
-		</div>
-	</div>
-</template>

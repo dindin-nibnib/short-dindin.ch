591d45829055155ce9dac17153ea159fb5576c4c
Odin Beuchat <odin.beuchat@divtec.ch>
2022-11-21 01:01:39 +0100

    added tailwind

--- src/id.tsx
+++ src/id.tsx
@@ -4,10 +4,22 @@ import {
 	useParams,
 } from "react-router-dom";
 
+const links = new Map([
+	["vsc", "https://code.visualstudio.com/"],
+	["pwned", "https://haveibeenpwned.com/"],
+	["mld", "https://drawsql.app/"],
+	["rr", "https://www.youtube.com/watch?v=a3Z7zEc7AXQ"],
+]);
+
 export default function Id() {
+	const { id } = useParams();
+	window.location.href = links.get(id || "") || "/";
+
 	return ( 
-	<>
-		{useParams().id}
-	</> 
+		<div
+			className="bg-slate-300 dark:bg-slate-800 dark:text-white h-screen flex flex-row items-center justify-center"
+		>
+			<h1>Redirecting...</h1>
+		</div>
 	);
 }
